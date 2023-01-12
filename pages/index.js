0c1cc8c763554ec6c81cf63f60b345ffac98786d
Ramon Orraca <dev@ramonorraca.com>
2023-01-11 21:13:53 -0600

    Add assets and metadata.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+
+const ProfilePicture = () => (
+  <div style={{ borderRadius: '45%', overflow: 'hidden' }}>
+    <Image
+      src="/images/profile.jpg"
+      height={400}
+      width={400}
+      alt="Ramón Orraca"
+    />
+  </div>
+);
 
 export default function Home() {
   return (
@@ -11,6 +23,8 @@ export default function Home() {
       </Head>
 
       <main>
+        <ProfilePicture />
+
         <h1 className="title">
           Read <Link href="/posts/first-post">this page!</Link>
         </h1>
@@ -30,18 +44,12 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+          <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card} >
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,23 @@
+import Head from 'next/head';
 import Link from 'next/link';
+// import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         // The empty tag is shorthand for <React.Fragment>, allowing you to have multiple top-most elements without wrapping further HTML.
         <>
+            <Head>
+                <title>First Post</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            // Example on how to add third-party scripts.
+            {/* <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload" // When the third-party script should load.
+                onLoad={() => // Run any JavaScript code immediately after the script has finished loading.
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                }
+            /> */}
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
